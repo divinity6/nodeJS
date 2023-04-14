3b22994c9a78677e29233c6f9f3651c40fac3359
divinity6 <divinity666@naver.com>
2023-04-14 23:36:38 +0900

    [ feature ][ DynamicContent > DataSharing ]요청_및_사용자간_데이터공유

---
+++ 05DynamicContent/src/app.js
@@ -0,0 +1,38 @@
+const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
+const path = require("path");
+
+const app = express();
+
+/**
+ * - 내보낸 router 파일을 import
+ */
+const adminData = require( './routes/admin.js' );
+
+const shopRoutes = require( './routes/shop.js' );
+
+/**
+ * - 본문 해석 미들웨어
+ *
+ * - urlencoded 메서드는 내부에서 next 를 호출하여
+ *
+ * - 다음 라우팅 미들웨어를 실행하도록 해준다
+ */
+app.use( bodyParser.urlencoded({ extended : false } ) );
+
+app.use( express.static( path.join( __dirname , 'public' ) ) );
+
+app.use( shopRoutes );
+
+app.use( '/admin' , adminData.routes );
+
+app.use( ( req , res , next ) => {
+    res.status( 404 ).sendFile( path.join( __dirname , 'views' , '404.html' ) );
+} );
+
+/**
+ * - express 내부에서 createServer 를 해서 서버를 만들어준다
+ *
+ * @param { number } port - 해당 정보를 수신할 port
+ */
+app.listen( 3000 );
---
+++ 05DynamicContent/src/routes/shop.js
@@ -0,0 +1,19 @@
+/**
+ * - 사용자가 보게될 내용
+ */
+const path = require( 'path' );
+
+const express = require( 'express' );
+
+const rootDir = require( '../util/path.js' );
+
+const adminData = require( './admin.js' );
+
+const router = express.Router();
+
+router.get( '/' , ( req , res , next )=> {
+    console.log( "shop.js" , adminData.products )
+    res.sendFile( path.join( rootDir , 'views' , 'shop.html' ) );
+} );
+
+module.exports = router;
