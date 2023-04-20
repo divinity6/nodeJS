d5851f9ff00469b9d6d717468ff87f875c94c9d8
divinity6 <divinity666@naver.com>
2023-04-20 21:30:16 +0900

    [ feature ][ DynamicContent > Pug ]Pug_html_작성

--- 05DynamicContent/src/app.js
+++ 05DynamicContent/src/app.js
@@ -38,7 +38,8 @@ app.use( shopRoutes );
 app.use( '/admin' , adminData.routes );
 
 app.use( ( req , res , next ) => {
-    res.status( 404 ).sendFile( path.join( __dirname , 'views' , '404.html' ) );
+    // res.status( 404 ).sendFile( path.join( __dirname , 'views' , '404.html' ) );
+    res.status( 404 ).render( '404' , { pageTitle : 'Page Not Found' } );
 } );
 
 /**
