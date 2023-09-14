c4bb59f1f65aa6827146d38857fe3e5acd122ed3
divinity6 <divinity666@naver.com>
2023-09-15 00:00:13 +0900

    [ init ][ RestAPI > REST_API_structure ]RestAPI_형식으로_구조_재설정

---
+++ 21RestAPI/src/controllers/feed.js
@@ -0,0 +1,9 @@
+/**
+ * - 게시물들을 반환하는 controller
+ * @param req
+ * @param res
+ * @param next
+ */
+exports.getPosts = ( req , res , next ) => {
+
+}
--- 21RestAPI/src/util/path.js
+++
@@ -1,6 +0,0 @@
-const path = require( 'path' );
-
-/**
- * - 파일 경로를 추출하는 helper 함수
- */
-module.exports = path.dirname( process.mainModule.filename );
